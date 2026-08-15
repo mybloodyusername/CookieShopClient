001e10f61742b4c1d909a9abf0adb20922327b06
mybloodyusername <y.sanaei94@gmail.com>
2026-08-16 00:27:21 +0330

    fix: role guard

--- src/app/guards/role.guard.ts
+++ src/app/guards/role.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, RedirectCommand, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { CurrentUserService } from '../services/current-user';
 import { toObservable } from '@angular/core/rxjs-interop';
@@ -7,9 +7,12 @@ import { switchMap } from 'rxjs';
 
 export const roleGuard = (role: 'customer' | 'admin'): CanActivateFn => {
   return (route, state) => {
+    const router = inject(Router);
     const currentUserService = inject(CurrentUserService);
+    const redirect = new RedirectCommand(router.parseUrl(''));
     const me = currentUserService.me;
     const isLoading = me.isLoading();
+
     if (isLoading) {
       const value$ = toObservable(me.value);
       const status$ = toObservable(me.status);
@@ -17,12 +20,12 @@ export const roleGuard = (role: 'customer' | 'admin'): CanActivateFn => {
         filter((status) => status == 'resolved'),
         switchMap(() => value$),
         map((res) =>
-          res && res.role ? res.role.toLowerCase().includes(role.toLowerCase()) : false,
+          res && res.role ? res.role.toLowerCase().includes(role.toLowerCase()) : redirect,
         ),
       );
     }
     const meValue = me.value();
-    if (!meValue || !meValue.role) return false;
+    if (!meValue || !meValue.role) return redirect;
     return meValue.role.toLowerCase().includes(role.toLowerCase());
   };
 };
